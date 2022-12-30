480038d383b8c956b72779468b3a8b9a0dc42901
Devin Riley <devin.riley144@gmail.com>
2022-12-30 10:52:08 -0600

    created number picking game

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,7 +4,24 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var r = new Random();
+            var favNumber = r.Next(1, 10);
+            Console.WriteLine("Enter a number between 1 and 10");
+            var userInput = int.Parse(Console.ReadLine());
+
+            if (userInput < favNumber)
+            {
+                Console.WriteLine("I'm sorry, thats too low!");
+            }
+            else if (userInput > favNumber)
+            {
+                Console.WriteLine("I'm sorry, too high!");
+            }
+            else 
+            {
+                Console.WriteLine("Congratulations, you guessed correct!!");
+            }            
+            
         }
     }
 }
